c01e5fd11d996344fcb576d505beaad4a0368b94
Tianle Chen <ericchentianle@gmail.com>
2015-09-30 12:13:24 -0700

    optimize pizza.html

--- views/js/main.js
+++ views/js/main.js
@@ -450,9 +450,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
+    var getPizzaElem = document.querySelector(".randomPizzaContainer");
+    var dx = determineDx(getPizzaElem, size);
+    var newwidth = (getPizzaElem.offsetWidth + dx) + 'px';
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
@@ -502,10 +503,15 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
+  var scrollPosition = document.body.scrollTop / 1250;
+  var phase = [];
+  for (var j = 0; j < 5; j++) {
+    phase[j] = Math.sin(scrollPosition + (j % 5));
+  }
+
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    items[i].style.left = items[i].basicLeft + 100 * phase[i % 5] + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
